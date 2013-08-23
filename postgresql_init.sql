c350c1cec6baa2b0ad4f75bb1afdd0c538a1604b
glejeune <gregoire.lejeune@free.fr>
2013-08-23 19:53:02 +0200

    Installation instructions

---
+++ postgresql_init.sql
@@ -0,0 +1,4 @@
+CREATE USER rubyfrance WITH PASSWORD 'rubyfrance';
+CREATE DATABASE rubyfrance_development OWNER rubyfrance;
+CREATE DATABASE rubyfrance_test OWNER rubyfrance;
+CREATE DATABASE rubyfrance_production OWNER rubyfrance;
